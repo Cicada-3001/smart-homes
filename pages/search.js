aa0adc6d91b14d4c9cabd0d8e5f0305c2217eb3b
Pablo Picasso Sancho <cicada3319@gmail.com>
2023-07-13 09:58:10 +0300

    initial-commit

--- pages/search.js
+++ pages/search.js
@@ -48,16 +48,16 @@ const Search = ({ properties }) => {
 };
 
 export async function getServerSideProps({ query }) {
-  const purpose = query.purpose || 'for-rent';
-  const rentFrequency = query.rentFrequency || 'yearly';
-  const minPrice = query.minPrice || '0';
-  const maxPrice = query.maxPrice || '1000000';
-  const roomsMin = query.roomsMin || '0';
-  const bathsMin = query.bathsMin || '0';
-  const sort = query.sort || 'price-desc';
-  const areaMax = query.areaMax || '35000';
-  const locationExternalIDs = query.locationExternalIDs || '5002';
-  const categoryExternalID = query.categoryExternalID || '4';
+    const purpose = query.purpose || 'for-rent';
+    const rentFrequency = query.rentFrequency || 'yearly';
+    const minPrice = query.minPrice || '0';
+    const maxPrice = query.maxPrice || '1000000';
+    const roomsMin = query.roomsMin || '0';
+    const bathsMin = query.bathsMin || '0';
+    const sort = query.sort || 'price-desc';
+    const areaMax = query.areaMax || '35000';
+    const locationExternalIDs = query.locationExternalIDs || '5002';
+    const categoryExternalID = query.categoryExternalID || '4';
 
   const data = await fetchApi(`${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`);
 
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get((url), {
     headers: {
       'x-rapidapi-host': 'bayut.p.rapidapi.com',
-      'x-rapidapi-key': process.env.NEXT_PUBLIC_RAPID_API_KEY ,
+      'x-rapidapi-key': "2465e156ddmshbc6234b1134ff03p15f91fjsna2ca23a09c46" ,
     },
   });
     
